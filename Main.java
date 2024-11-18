18b317b783d356725b1cf6ebc2f9b0f646b72a5e
TeaFred <fhtruong100@student.hbuhsd.edu>
2024-11-18 19:24:37 +0000

    saved

--- Main.java
+++ Main.java
@@ -1,25 +1,54 @@
+package mycustompackage;
 import java.util.Scanner;
 
 public class Main
 {
   public static void main(String[] args)
   {
-    // Make sure to test your methods here
+    //printNTimes("code",3);
+    //printNums();
+    uprightNumberTriangle(7);
   }
 
   public static void printNTimes(String word, int N)
   {
-    // code solution here
+    for (int i = 0; i < word.length(); i++)
+    {
+      for (int c = 0; c < N; c++)
+      {
+      String letter = word.substring(i,i+1);
+      System.out.print(letter);
+      }
+    }
   }
 
   public static void printNums()
   {
-    // code solution here
+    int nUm = 0;
+   for (int i = 10; i > 0; i--)
+   {
+    nUm = i;
+    for (int c = i; c > 0; c--)
+    {
+      System.out.println(nUm + " ");
+    }
+   }
   }
 
   public static void uprightNumberTriangle(int N)
   {
-    // code solution here
+    int nUm2;
+    int counter = 0;
+    for (int i = 0; i < N+1; i++)
+    {
+      for (int c = 1; c < counter+1; c++)
+      {
+        nUm2 = c;
+        System.out.print(nUm2 + " ");
+      }
+      counter++;
+      System.out.println();
+    }
   }
 
   public static void starTree()
